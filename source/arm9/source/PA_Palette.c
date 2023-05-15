b592bd661daa9b19958732c5039ad34ce342aeff
AerGlass <86316669+AerGlass@users.noreply.github.com>
2023-05-14 21:04:13 -0400

    update

--- source/arm9/source/PA_Palette.c
+++ source/arm9/source/PA_Palette.c
@@ -17,11 +17,11 @@ void PA_InitSpriteExtPal(void) {
 void PA_LoadSpriteExtPal(u8 screen, u16 palette_number, void* palette) {
 	if (screen == 0) {
 		vramSetBankG(VRAM_G_LCD);  // On passe en mode LCD pour pouvoir ecrire dessus, on reviendre en palette apres
-		DMA_Copy(palette, VRAM_G + (palette_number << 9), 256, DMA_16NOW);
+		dmaCopy(palette,VRAM_G_EXT_SPR_PALETTE[palette_number],256);
 		vramSetBankG(VRAM_G_SPRITE_EXT_PALETTE);
 	} else {
 		vramSetBankI(VRAM_I_LCD);  // On passe en mode LCD pour pouvoir ecrire dessus, on reviendre en palette apres
-		DMA_Copy(palette, VRAM_I + (palette_number << 9), 256, DMA_16NOW);
+		dmaCopy(palette,VRAM_I_EXT_SPR_PALETTE[palette_number],256);
 		vramSetBankI(VRAM_I_SUB_SPRITE_EXT_PALETTE);
 	}
 }
@@ -47,11 +47,15 @@ void PA_InitBgExtPal(void) {
 void PA_LoadBgPalN(u8 screen, u8 bg_number, u8 pal_number, void* palette) {
 	if (screen == 0) {
 		vramSetBankE(VRAM_E_LCD);  // On passe en mode LCD pour pouvoir ecrire dessus, on reviendre en palette apres
-		DMA_Copy(palette, VRAM_E + (bg_number << 13) + (pal_number << 9), 256, DMA_16NOW);
+		//DMA_Copy(palette, VRAM_E + (bg_number << 13) + (pal_number << 9), 256, DMA_16NOW);
+		//New code
+		dmaCopy(palette,&VRAM_E_EXT_PALETTE[bg_number][pal_number],256);
 		vramSetBankE(VRAM_E_BG_EXT_PALETTE);
 	} else {
 		vramSetBankH(VRAM_H_LCD);  // On passe en mode LCD pour pouvoir ecrire dessus, on reviendre en palette apres
-		DMA_Copy(palette, VRAM_H + (bg_number << 13) + (pal_number << 9), 256, DMA_16NOW);
+		//DMA_Copy(palette, VRAM_H + (bg_number << 13) + (pal_number << 9), 256, DMA_16NOW);
+		//new code
+		dmaCopy(palette,&VRAM_H_EXT_PALETTE[bg_number][pal_number],256);
 		vramSetBankH(VRAM_H_SUB_BG_EXT_PALETTE);
 	}
 }
